c83584da98430a58b4fd7292e3eb649194640b1d
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-12 11:45:06 +0300

    example 1

--- AlgorithmCourse/Program.cs
+++ AlgorithmCourse/Program.cs
@@ -10,11 +10,34 @@ namespace AlgorithmCourse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("İsim giriniz.");
-            string isim = Console.ReadLine();
-            Console.WriteLine("Hoşgeldin {0}",isim);
+            //Intro();
+
+            Console.WriteLine("Birici sayıyı giriniz.");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz.");
+            int sayi2 = int.Parse(Console.ReadLine());
+
+            if (sayi1>sayi2)
+            {
+                Console.WriteLine("Büyük sayı: {0}",sayi1);
+            }
+            else if (sayi2>sayi1)
+            {
+                Console.WriteLine("Büyük sayı: {0}", sayi2);
+            }
+            else
+            {
+                Console.WriteLine("Sayılar eşit.");
+            }
 
             Console.ReadLine();
+        }
+
+        private static void Intro()
+        {
+            Console.WriteLine("İsim giriniz.");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin {0}", isim);
         }
     }
 }
