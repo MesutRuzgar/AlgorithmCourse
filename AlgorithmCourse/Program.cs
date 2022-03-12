9c200b7e10ac2c96d6074857b8948f283fa6ca44
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-12 11:59:04 +0300

    switch case example

--- AlgorithmCourse/Program.cs
+++ AlgorithmCourse/Program.cs
@@ -13,7 +13,41 @@ namespace AlgorithmCourse
             //Intro();
             //Example1();
             //Example2();
-            Example3();
+            //Example3();
+           
+            Console.WriteLine("Haftanın kaçıncı günü?");
+            int gun = Convert.ToInt32(Console.ReadLine());
+
+            switch (gun)
+            {
+                case 1:
+                    Console.WriteLine("Pazartesi");
+                    break;
+                case 2:
+                    Console.WriteLine("Salı");
+                    break;
+                case 3:
+                    Console.WriteLine("Çarşamba");
+                    break;
+                case 4:
+                    Console.WriteLine("Perşembe");
+                    break;
+                case 5:
+                    Console.WriteLine("Cuma");
+                    break;
+                case 6:
+                    Console.WriteLine("Cumartesi");
+                    break;
+                case 7:
+                    Console.WriteLine("Pazar");
+                    break;
+
+                default:
+                    Console.WriteLine("Hatalı giriş!");
+                   
+                    break;
+            }
+
 
             Console.ReadLine();
         }
