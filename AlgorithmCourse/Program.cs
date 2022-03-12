968b9e6059322b212ba7da5a1dd0cd0aa55bbc01
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-12 11:31:07 +0300

    Add project files.

---
+++ AlgorithmCourse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsim giriniz.");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Hoşgeldin {0}",isim);
+
+            Console.ReadLine();
+        }
+    }
+}
